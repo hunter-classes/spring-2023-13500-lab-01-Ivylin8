0ff960836ab1c76147a96311d40a552198ef1175
Ivy Lin <>
2023-01-29 05:43:03 +0000

    update smaller cpp

---
+++ smaller.cpp
@@ -0,0 +1,19 @@
+// Lab : Write a program smaller.cpp that asks the user to input two integer numbers and prints out the smaller of the two.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int x, y;
+cout << "Enter two numbers" << endl;
+cin >> x >> y;
+
+if (x > y)
+cout << y << "is smaller." << endl;
+
+if (y > x)
+cout << x << "is smaller." << endl;
+  
+return 0;  
+}
