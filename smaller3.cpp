5fffc9b8cd51a4cbb57e71047a96f8c24da41a82
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-01-31 22:14:04 +0000

    Update B

--- smaller3.cpp
+++ smaller3.cpp
@@ -1,24 +1,23 @@
 //Task B. Find the smaller of three integers.
 
 #include <iostream>
-using namespace std;
 
 int main() 
 {
 int x, y, z;
-cout << "Enter a number: ";
-cin >> x;
-cout << "Enter a number: ";
-cin >> y;
-cout << "Enter a number: ";
-cin >> x;
+std::cout << "Enter a number: ";
+std::cin >> x;
+std::cout << "Enter a number: ";
+std::cin >> y;
+std::cout << "Enter a number: ";
+std::cin >> x;
 
 if (x <= y && x <= z)
-    cout << x << " is the smallest";
+    std::cout << x << " is the smallest";
 if (y <= x && y <= z)
-    cout << y << " is the smallest"; 
+    std::cout << y << " is the smallest"; 
 if (z <= x && z <= y)
-    cout << z << " is the smallest";
+    std::cout << z << " is the smallest";
 
 return 0;
 }
