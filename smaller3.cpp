f60ac94cda82d910cc08792e4d5f40acf5f02204
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-02-01 21:51:12 +0000

    update notes

--- smaller3.cpp
+++ smaller3.cpp
@@ -4,16 +4,17 @@
 
 int main() 
 {
-int x, y, z;
-std::cout << "Enter a number: ";
+int x, y, z; //setup variables
+std::cout << "Enter a number: "; //input from user
 std::cin >> x;
 std::cout << "Enter a number: ";
 std::cin >> y;
 std::cout << "Enter a number: ";
 std::cin >> x;
-
+//create if else statements 
 if (x <= y && x <= z)
     std::cout << x << " is the smallest";
+  //update the input 
 if (y <= x && y <= z)
     std::cout << y << " is the smallest"; 
 if (z <= x && z <= y)
