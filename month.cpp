b476fe6f648e3bce2cfa135cc41f8a19b3807769
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-02-01 22:46:48 +0000

    added month.cpp

---
+++ month.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int month = 0;
+  int year = 0;
+  int days;
+  cout << "Input a month in numbers: ";
+  cin >> month;
+  cout << "Input a year: ";
+  cin >> year;
+
+  if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+    days = 31;
+
+  else if (month == 4 || month == 6 || month == 11 || month == 9)
+    days = 30;
+  else if ( month == 2 && ((year %4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)))
+  {
+    return 29;
+  }
+  else
+  {
+    return 28;
+  }
+
+  cout << "There is " << days << " days in the month."<<endl;
+  
+return 0;
+  
+    
+  }
