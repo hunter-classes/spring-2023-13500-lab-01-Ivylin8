935d0ab0aa3526c82deeecd42dba5caac9c9daf4
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-02-07 00:20:34 +0000

    update part a

--- valid.cpp
+++ valid.cpp
@@ -8,21 +8,20 @@ Ask users to input an intger thats between 0-100. Will ask user to re-enter if o
 */
 
 #include <iostream>
-using namespace std;
 
 int main() 
 {
   int num;
-  cout << "please enter an integer from 0 to 100: ";
-  cin >> num;
+  std::cout << "please enter an integer from 0 to 100: ";
+  std::cin >> num;
   
   while(num <= 0 || num >=100){
-    cout << "Please re-enter: ";
-    cin >> num;
+    std::cout << "Please re-enter: ";
+    std::cin >> num;
     }
 
   int result = num * num;
-  cout << "Square of " << num << " is " << result;
+  std::cout << "Square of " << num << " is " << result;
     
   return 0;
   }
